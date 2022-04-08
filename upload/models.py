6ca4f21086a69cc3d0b2315e10a987a5aa3f0ad0
hq <2948054774@qq.com>
2022-04-08 13:00:45 +0800

    rm

--- upload/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Content(models.Model):
-
-    picture = models.FileField(upload_to='picture')
-    
--- user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('reg',views.reg_view),
-    
-    path('login',views.login_view),
-    path('logout',views.logout)
-]
